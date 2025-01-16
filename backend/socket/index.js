648dbe7d4a0b1b11424d52a42f42d1bed14e9f7f
Varad_Kale21 <126102629+darav321@users.noreply.github.com>
2025-01-17 02:28:27 +0530

    Update index.js

--- backend/socket/index.js
+++ backend/socket/index.js
@@ -10,7 +10,7 @@ import { getConversation } from '../helpers/getConversation.js'
 export const server = http.createServer(app)
 const io = new Server(server, {
     cors : {
-        origin: "https://chatmore-1.onrender.com",
+        origin: "*",
         credentials : true
     }
 })
