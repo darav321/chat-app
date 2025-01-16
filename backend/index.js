9db8bdc175c89337fd664fd2f26bbf2f969a76bc
Varad <varadkale04@gmail.com>
2025-01-16 18:56:34 +0530

    updated deployed urls

--- backend/index.js
+++ backend/index.js
@@ -8,7 +8,7 @@ import { server, app } from "./socket/index.js"
 
 dotenv.config()
 app.use(cors({
-    origin: "http://localhost:5173",
+    origin: "https://chat-more-zwg6.vercel.app/",
     credentials : true
 }))
 app.use(express.json())
--- backend/socket/index.js
+++ backend/socket/index.js
@@ -10,7 +10,7 @@ import { getConversation } from '../helpers/getConversation.js'
 export const server = http.createServer(app)
 const io = new Server(server, {
     cors : {
-        origin: "http://localhost:5173",
+        origin: "https://chat-more-zwg6.vercel.app/",
         credentials : true
     }
 })
